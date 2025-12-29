99bae04ad8db833d06d4e28870393c9b6a2e35fa
Oliveira Hambaena <167088414+OliveiraHambaena@users.noreply.github.com>
2025-12-29 04:21:39 +0200

    Update Resume
    IVe uploaded my updated Resume

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -38,7 +38,7 @@ export default function Contact({ current, onNav }) {
             <div className="contact-icon">📄</div>
             <strong>Resume</strong>
             <a
-              href="https://drive.google.com/file/d/1MMfBadHI118D61QyxZ5vjhf0Jx6wBlEp/view?usp=drive_link"
+              href="https://drive.google.com/file/d/15OykfHskrYOhGpQ62TsMNZjJs9dwurLa/view?usp=drive_link"
               target="_blank"
               rel="noreferrer"
               className="resume-btn"
